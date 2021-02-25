6007826cd40a62ac1af6a7bcfc9a8e19a5fa987a
hanliong-looi <hanliong99@gmail.com>
2021-02-25 16:51:39 +0800

    rpc

---
+++ src/main/java/cz4013/shared/rpc/RawMessage.java
@@ -0,0 +1,25 @@
+package cz4013.shared.rpc;
+
+import cz4013.shared.container.PooledByteBuffer;
+
+import java.net.SocketAddress;
+
+/**
+ * A UDP datagram: its sender and its content.
+ */
+System.out.println("test");
+System.out.println("test");
+public class RawMessage implements AutoCloseable {
+  public SocketAddress remote;
+  public PooledByteBuffer payload;
+
+  public RawMessage(SocketAddress remote, PooledByteBuffer payload) {
+    this.remote = remote;
+    this.payload = payload;
+  }
+
+  @Override
+  public void close() {
+    payload.close();
+  }
+}
