ced3692e5eb71413875b136fc375444eefa38877
hanliong-looi <hanliong99@gmail.com>
2021-02-25 16:47:25 +0800

    test

---
+++ src/main/java/cz4013/shared/response/ResponseHeader.java
@@ -0,0 +1,35 @@
+package cz4013.shared.response;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Header of a response: the corresponding request's ID and the response's status code.
+ */
+System.out.println("test");
+public class ResponseHeader {
+  public UUID uuid;
+  public Status status;
+
+  public ResponseHeader() {}
+
+  public ResponseHeader(UUID uuid, Status status) {
+    this.uuid = uuid;
+    this.status = status;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ResponseHeader)) return false;
+    ResponseHeader header = (ResponseHeader) o;
+    return Objects.equals(uuid, header.uuid) &&
+      status == header.status;
+  }
+
+  @Override
+  public String toString() {
+    return "ResponseHeader(" + uuid + ", " + status + ")";
+  }
+}
+
